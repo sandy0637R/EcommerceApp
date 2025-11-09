d3008ad963fc8891a489c766953dacc97166cd2a
sandy0637R <sandeshraut842@gmail.com>
2025-11-09 15:42:16 +0530

    fixed deployment errors

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ["react", "react-dom", "react-router-dom"],
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom", "react-router-dom"],
-  },
-});
+})
